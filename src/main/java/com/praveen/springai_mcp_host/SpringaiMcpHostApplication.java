85b46b2e36dcc8127532e942d207b4e208c8b6af
praveenkumar09 <tpkprav@gmail.com>
2025-11-16 11:42:47 +0800

    Initial commit

---
+++ src/main/java/com/praveen/springai_mcp_host/SpringaiMcpHostApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.springai_mcp_host;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiMcpHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiMcpHostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/springai_mcp_host/SpringaiMcpHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.springai_mcp_host;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiMcpHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
