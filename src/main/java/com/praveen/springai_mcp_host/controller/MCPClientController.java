de53797f7f51890c07898686e5279d3b401b0243
praveenkumar09 <tpkprav@gmail.com>
2025-11-16 12:16:05 +0800

    Add MCP properties and mcp server json

---
+++ src/main/java/com/praveen/springai_mcp_host/controller/MCPClientController.java
@@ -0,0 +1,34 @@
+package com.praveen.springai_mcp_host.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MCPClientController {
+
+    private final ChatClient chatClient;
+
+    public MCPClientController(
+            ChatClient.Builder chatClientBuilder
+    ){
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(
+            @RequestParam("message") String message
+    ){
+        return chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+}
